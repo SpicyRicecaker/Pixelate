7d55da8ec0fa6b1e80b06e601d5bae27a8f8ad14
SpicyRicecaker <awesomenessisgolden@gmail.com>
2020-07-24 03:13:27 -0700

    File cleanup, resolution changes

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const {app, BrowserWindow} = require('electron');
 function createWindow(){
     //Makes the browser window
     const win = new BrowserWindow({
-        width: 1280,
-        height: 720,
+        width: 1366,
+        height: 768,
         webPreferences: {
             nodeIntegration: true
         }
