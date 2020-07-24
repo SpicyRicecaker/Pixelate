7d85d2e885e3bc9e22c59b77253680c135b58c80
SpicyRicecaker <awesomenessisgolden@gmail.com>
2020-07-24 05:28:02 -0700

    Cleaned up main.js comments and error

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
-  //Makes the browser window
+  // Makes the browser window
   const win = new BrowserWindow({
     width: 1366,
     height: 768,
@@ -10,26 +10,30 @@ function createWindow() {
     },
   });
 
-  //Renders the index.html of the app
-  win.loadFile("index.html");
+  // Renders the index.html of the app
+  win.loadFile('index.html');
 
-  //Opens the dev tools????
+  // Opens the dev tools????
   win.webContents.openDevTools();
 }
 
-// This will be called after Electron finishes initiation and can actually create browser windows... Some APIs can only be used after this event occurs
+// This will be called after Electron finishes initiation and can actually create
+// browser windows...
+// Some APIs can only be used after this event occurs
 app.whenReady().then(createWindow);
 
-//Exit app when all windows are closed, unless on mac, because mac keeps the apps open (mac users omegalul)
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
+// Exit app when all windows are closed, unless on mac, because
+// mac keeps the apps open (mac users omegalul)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on("activate", () => {
-  //On mac, re create a window in the app when the dock icon is clicked and there are no other windows open
-  if (BrowserSWindow.getAllWindows().length === 0) {
+app.on('activate', () => {
+  // On mac, re create a window in the app when the dock icon is clicked
+  // and there are no other windows open
+  if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
